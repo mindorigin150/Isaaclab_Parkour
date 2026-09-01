f0767fc5ca6c84693b6246f02d49b08eab888fe6
Xinyuan Li <1501454486@qq.com>
2026-09-01 17:45:07 +0800

    fix: use current keyboard unsubscribe API

--- parkour_isaaclab/envs/parkour_viewprot_camera_contoller.py
+++ parkour_isaaclab/envs/parkour_viewprot_camera_contoller.py
@@ -67,7 +67,7 @@ class ParkourViewportCameraController(ViewportCameraController):
 
     def __del__(self):
         """Release the keyboard interface."""
-        self._input.unsubscribe_from_keyboard_events(self._keyboard, self._keyboard_sub)
+        self._input.unsubscribe_to_keyboard_events(self._keyboard, self._keyboard_sub)
         self._keyboard_sub = None
 
     def _on_keyboard_event(self, event, *args, **kwargs):
